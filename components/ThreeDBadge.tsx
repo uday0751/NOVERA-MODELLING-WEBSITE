bef1b42393d003897fcddf6d16210e8f31fcad00
uday0751 <uday0751@users.noreply.github.com>
2026-08-28 21:37:36 +0530

    Shift HIGH FASHION agency badge a bit lower using translate-y-7

--- components/ThreeDBadge.tsx
+++ components/ThreeDBadge.tsx
@@ -55,7 +55,7 @@ export function ThreeDBadge() {
   }, []);
 
   return (
-    <div style={{ perspective: 1000 }} className="inline-block translate-y-3.5 whitespace-nowrap ml-auto">
+    <div style={{ perspective: 1000 }} className="inline-block translate-y-7 whitespace-nowrap ml-auto">
       <motion.div
         ref={badgeRef}
         onMouseMove={handleMouseMove}
